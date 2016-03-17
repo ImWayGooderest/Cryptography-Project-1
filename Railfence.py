ae4f199f832932186dabc8a8ac922858e8c6a51d
Trevor Hurt <fatalphoenixx@yahoo.com>
2016-03-17 01:21:31 -0700

    Added files via upload
    Railfence.py and cipher.py finished

--- Railfence.py
+++ Railfence.py
@@ -1,14 +1,80 @@
 from CipherInterface import CipherInterface
 
+
 class Railfence(CipherInterface):
-	def __init__(self):
-		self.key = ""
+    def __init__(self):
+        self.key = ""
 
-	def setKey(self, keyString):
-		pass
+    def setKey(self, keyString):
+        self.key = int(keyString)
 
-	def encrypt(self, plaintext):
-		pass
+    ###pass
 
-	def decrypt(self, ciphertext):
-		pass
+    def encrypt(self, plaintext):
+        ###pass
+        plaintext = plaintext.replace(" ", "")
+        ciphertext = []
+        myindex = 0
+        intplain = len(plaintext)
+        for i in range(0, self.key):
+            ciphertext.append("")
+
+        for i in range(0, intplain):
+            ciphertext[myindex] += plaintext[i]
+            myindex = (myindex + 1) % 3
+
+        return "".join(ciphertext)
+
+    def decrypt(self, ciphertext):
+        plaintextResult = ""
+        rails = self.key
+        cipherLen = len(ciphertext)
+        col = cipherLen // rails
+        extra = cipherLen % rails
+        plaintext = []
+        temp = 0
+        '''for i in range(0, rails):
+            plaintext.append("")'''
+
+        plaintext = ["" for i in range(cipherLen)]
+
+        print(plaintext)
+
+        if extra > 0:
+            for i in range(0, extra):
+                plaintext[i] = ciphertext[0:(col + 1)]
+                ciphertext = ciphertext[(col + 1):]
+
+            for i in range(extra, rails):
+                plaintext[i] = ciphertext[0: col]
+                ciphertext = ciphertext[(col + 1):]
+                print(plaintext)
+
+            test = 0
+            for i in range(0, col+1):
+                for j in range(0, rails):
+                    plaintextResult += plaintext[j][i]
+                    if(test == cipherLen - 1):
+                        break
+                    test += 1
+                    ####print(plaintextResult)
+
+            ####print(plaintextResult)
+
+            return plaintextResult
+        else:
+            for i in range(0, rails):
+                plaintext[i] = ciphertext[0: col]
+                ciphertext = ciphertext[(col):]
+                print(plaintext)
+
+            breakNum = 0
+            for i in range(0, col):
+                for j in range(0, rails):
+                    plaintextResult += plaintext[j][i]
+                    if(breakNum == cipherLen):
+                        break
+                    breakNum += 1
+                    ###print(plaintextResult)
+
+            return plaintextResult
--- cipher.py
+++ cipher.py
@@ -1,13 +1,90 @@
-import Playfair,RowTransposition,Railfence, Vigenre, Caesar, Hill
+import Railfence, RowTransposition, Playfair, Vigenre,Caesar
+import sys
+
+def handleInput(cipherName, inputKey, encDec, inputFile, outputFile):	###function to handle user input
+	iFile = open(inputFile).read()
+	oFile = open(outputFile, "w")
+
+	if(cipherName == "PLF"):	###Playfair cipher
+		myCipher = Playfair.Playfair()
+		myCipher.setKey(inputKey)
+		if(encDec == "ENC"):
+			cipherText = myCipher.encrypt(iFile)
+			oFile.write(cipherText)
+		else:
+			plainText = myCipher.decrypt(iFile)
+			oFile.write(plainText)
+
+	elif(cipherName == "RTS"):	###Row Transposition cipher
+		myCipher = RowTransposition.RowTransposition()
+		myCipher.setKey(inputKey)
+		if(encDec == "ENC"):
+			cipherText = myCipher.encrypt(iFile)
+			oFile.write(cipherText)
+		else:
+			plainText = myCipher.decrypt(iFile)
+			oFile.write(plainText)
+
+	elif(cipherName == "RFC"):	###Railfence cipher
+		myCipher = Railfence.Railfence()
+		myCipher.setKey(inputKey)
+		if(encDec == "ENC"):
+			cipherText = myCipher.encrypt(iFile)
+			oFile.write(cipherText)
+		elif(encDec == "DEC"):
+			plainText = myCipher.decrypt(iFile)
+			oFile.write(plainText)
+			###oFile.write(plainText)
+			###print(iFile)
+			###print(iFile[(5+1):])
+			###print(myCipher.decrypt(iFile))
+
+	elif(cipherName == "VIG"):	###Vigenre cipher
+		myCipher = Vigenre.Vigenre()
+		myCipher.setKey(inputKey)
+		if(encDec == "ENC"):
+			cipherText = myCipher.encrypt(iFile)
+			oFile.write(cipherText)
+		else:
+			###plainText = myCipher.decrypt(iFile)
+			###oFile.write(plainText)
+			print(iFile)
+			print(myCipher.decrypt(iFile))
+
+	elif(cipherName == "CES"):	###Caesar cipher
+		myCipher = Caesar.Caesar()
+		myCipher.setKey(inputKey)
+		if(encDec == "ENC"):
+			cipherText = myCipher.encrypt(iFile)
+			oFile.write(cipherText)
+		else:
+			plainText = myCipher.decrypt(iFile)
+			oFile.write(plainText)
+
 
 def main():
 	#below is for testing the caesar cipher
-	testKey = 1
-	cipher = Caesar.Caesar()
+	'''testKey = 2
+	cipher = Railfence.Railfence()
 	cipher.setKey(testKey)
-	ciphertext = cipher.encrypt("this is a test")
-	print(ciphertext)
-	print(cipher.decrypt(ciphertext))
+	ciphertext = cipher.encrypt("meet me after the toga party")
+	print(ciphertext)'''
+	###print(cipher.decrypt(ciphertext))
+
+	'''cipherName = sys.argv[1]
+	inputKey = sys.argv[2]
+	encrypDecryp = sys.argv[3]
+	inputFile = sys.argv[4]
+	outputFile = sys.argv[5]
+	myFile = open(inputFile).read()'''
+
+	handleInput(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+	'''if(cipherName == "PLF"):
+		myCipher = Playfair.Playfair()
+		myCipher.setKey(inputKey)'''
+
+
+
 
 if __name__ == "__main__":
 	main()
