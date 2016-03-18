d1ff03b8b75d2cd432704a577cdc02ec40a6516e
AlbertoGo <Otrebla117@csu.fullerton.edu>
2016-03-18 15:27:19 -0700

    Fixed

--- Playfair.py
+++ Playfair.py
@@ -5,7 +5,7 @@ class Playfair(CipherInterface):
 		self.key = ""
 		self.alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 		self.matrix = [[],[],[],[],[]]
-		for x in matrix:
+		for x in self.matrix:
 			for y in range(0, 5):
 				x.append('')
 
@@ -14,22 +14,29 @@ class Playfair(CipherInterface):
 		self.key = self.key.replace('J', 'I')
 		count = 0
 		alphCount = 0
-		for x in self.matrix:
-			for y in x:
+
+		if self.matrix[0][0] != '':		#Clear matrix if not already clear
+			for x in range(0, 5):
+				for y in range(0, 5):
+					self.matrix[x][y] = ''
+
+		for x in range(0, 5):			#Populate Playfair matrix
+			for y in range(0, 5):
 				if count < len(self.key):
-					while self.exists(self.key[count]) and count < len(self.key):
+					while count < len(self.key) and self.exists(self.key[count]):
 						count += 1
 					if count < len(self.key):
-						y = self.key[count]
+						self.matrix[x][y] = self.key[count]
 				if count >= len(self.key):
-					while self.exists(self.alphabet[alphCount]) and alphCount < 25:
+					while alphCount < 25 and self.exists(self.alphabet[alphCount]):
 						alphCount += 1
 					if alphCount < 25:
-						y = self.alphabet[alphCount]
+						self.matrix[x][y] = self.alphabet[alphCount]
 
 
 	def encrypt(self, plaintext):
 		plaintext = plaintext.replace(" ","")
+		plaintext = plaintext.upper()
 		plaintext = plaintext.replace('J', 'I')
 		enc = []
 		count = 0
@@ -44,8 +51,8 @@ class Playfair(CipherInterface):
 			if char1 == char2:
 				char2 = 'X'
 				count -= 1
-			coord1 = getCoord(char1)
-			coord2 = getCoord(char2)
+			coord1 = self.getCoord(char1)
+			coord2 = self.getCoord(char2)
 
 			if coord1[0] == coord2[0]:
 				char1 = self.matrix[coord1[0]][(coord1[1]+1)%5]
@@ -55,16 +62,17 @@ class Playfair(CipherInterface):
 				char2 = self.matrix[(coord2[0]+1)%5][coord2[1]]
 			else:
 				char1 = self.matrix[coord1[0]][coord2[1]]
-				char2 = self.matrix[coord1[0]][coord2[1]]
+				char2 = self.matrix[coord2[0]][coord1[1]]
 			enc.append(char1)
 			enc.append(char2)
 
 			count += 2
-		return enc
+		return "".join(enc)
 
 
 	def decrypt(self, ciphertext):
 		ciphertext = ciphertext.replace(" ","")
+		ciphertext = ciphertext.upper()
 		ciphertext = ciphertext.replace('J', 'I')
 		dec = []
 		count = 0
@@ -73,8 +81,8 @@ class Playfair(CipherInterface):
 			char1 = ciphertext[count]
 			char2 = ciphertext[count+1]
 
-			coord1 = getCoord(char1)
-			coord2 = getCoord(char2)
+			coord1 = self.getCoord(char1)
+			coord2 = self.getCoord(char2)
 
 			if coord1[0] == coord2[0]:
 				char1 = self.matrix[coord1[0]][(coord1[1]-1)%5]
@@ -84,12 +92,12 @@ class Playfair(CipherInterface):
 				char2 = self.matrix[(coord2[0]-1)%5][coord2[1]]
 			else:
 				char1 = self.matrix[coord1[0]][coord2[1]]
-				char2 = self.matrix[coord1[0]][coord2[1]]
+				char2 = self.matrix[coord2[0]][coord1[1]]
 			dec.append(char1)
 			dec.append(char2)
 
 			count += 2
-		return dec
+		return "".join(dec)
 
 	def exists(self, target):
 		for x in self.matrix:
