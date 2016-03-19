87c14cd18f9d90f687610aee227264d3a0d5b5e9
Brendon Hollingsworth <tucaeg@yahoo.com>
2016-03-18 22:12:40 -0700

    progress on row transposition

--- RowTransposition.py
+++ RowTransposition.py
@@ -1,8 +1,10 @@
 from CipherInterface import CipherInterface
+import math
 
 class RowTransposition(CipherInterface):
 	def __init__(self):
 		self.key = []
+		self.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 	def setKey(self, keyString):
 		self.key = keyString.split()
@@ -12,10 +14,26 @@ class RowTransposition(CipherInterface):
 			if int(keyPart) != count:
 				return False
 			count += 1
-
 		return True
 	def encrypt(self, plaintext):
-		pass
+		numberOfCol = len(self.key)
+
+		textSize = len(plaintext)
+		numberOfRow = math.ceil(textSize/numberOfCol)
+		plaintext = plaintext.upper()
+		matrix = [["X" for x in range(numberOfCol)] for x in range(numberOfRow)] #matrix goes matrix[row][col]
+		cipherMatrix = matrix
+		count = 0
+
+		#fill matrix
+		for row in range(0, len(matrix)):
+			for col in range(0,len(matrix[row])):
+				if(count < len(plaintext)):
+					while(plaintext[count] not in self.alphabet and count < len(plaintext)): #skip char if not a letter
+						count += 1
+					matrix[row][col] = plaintext[count]
+					count += 1
+
 
 	def decrypt(self, ciphertext):
 		pass
