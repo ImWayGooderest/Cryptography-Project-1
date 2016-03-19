98f8d190074486b81dc9175e513bf4d9ee43aefe
MrSxky <zakashx@gmail.com>
2016-03-18 17:20:10 -0700

    Update MAC.py

--- MAC.py
+++ MAC.py
@@ -5,9 +5,6 @@ class (CipherInterface):
 def __init__(self):
     #plaintext for this problem
     #plaintext ="Vegeta is better than Goku!!"
-    #print ("the Plaintext is :")
-    #print (plaintext)
-    #print ("\n")
     self.key = {' ' :' ', 'a':'f' ,'b':'h' ,'c':'v' ,'d':'g' ,'e':'w' ,'f':'q' ,'g':'p' ,'h':'o' ,'i':'r' ,'j':'e' ,
          'k':'n' ,'l':'s' ,'m':'u' ,'n':'a' ,'o':'b' ,'p':'j' ,'q':'m' ,'r':'y' ,'s':'z' ,'t':'x' ,'u':'c' ,'v':'d' ,'w':'t' ,'x':'i' ,'y':'k' ,'z':'l', '!':'?' }
 
@@ -15,13 +12,16 @@ def __init__(self):
 
 
 def encrypt(self, plaintext):
-#To initiate empty string
     plaintext = plaintext.lower()
+    print ("the Plaintext is :")
+    print (plaintext)
+    print ("\n")
+    #To initiate empty string
     enc = []
     #loop for every letter of plaintext
     for item in plaintext:
         enc.append(mono[item])
-       #print title
+        #print title
         print ("the ciphertext is :")
         #http://www.pythonlearn.com/html-008/cfbook009.html reference
         #joining strings in a list
@@ -36,14 +36,14 @@ def encrypt(self, plaintext):
 def decrypt(self, ciphertext):
 	#initiate Empty string
     dec = []
-    #to store items after reverting the mono set
-    reverse = {}
+    #to store items to new mono set
+    new = {}
 
-    #loop to reverse the string
+    #loop to reverse the letters in the dictionary
     for k,v in mono.items():
         new[v] = k
 
-    #loop to append reverse's itemset to decrypt's stringset
+    #loop to append new itemset to dec's stringset
     for item in ciphertext:
         dec.append(new[item])
 
